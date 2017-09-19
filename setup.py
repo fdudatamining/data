0411ff597506b91269ca8e193278742c08dc739a
Daniel Clarke <u8sand@gmail.com>
2017-09-18 21:14:57 -0400

    Use MYSQL_ROOT_PASSWORD and create database

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
+import os
 import sys
 import importlib
 import sqlalchemy
 
 databases = {
   'test': 'sqlite:///test.sqlite3',
-  'install': 'mysql://root@localhost/',
+  'install': 'mysql://root:%s@127.0.0.1' % (os.environ['MYSQL_ROOT_PASSWORD']),
 }
 
 db = sys.argv[1]
 con = sqlalchemy.create_engine(databases.get(db, db))
+con.execute('create database if not exists datamining')
+con.execute('use datamining')
 
 for mod in reversed(['diagnosis', 'hospitals', 'practitioners']): # todo: programically find these
   print('Processing datamining.%s...' % (mod))
